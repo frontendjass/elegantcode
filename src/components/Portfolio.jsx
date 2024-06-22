ccec5d84e6d052e6cc082c5dfe3f049f47a9bb19
Jass <frontendjass@gmail.com>
2024-06-22 11:27:51 -0500

    Improved portfolio layout.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,16 +1,18 @@
-import Project from "./Project";
+import Project from './Project';
 
 export default function Portfolio() {
   return (
-    <section className='bg-swirl-200 rounded-t-3xl py-5' id="portfolio">
+    <section className='bg-swirl-200 rounded-t-3xl py-5' id='portfolio'>
       <header className='pl-5 pt-9'>
         <h2 className='font-lora uppercase text-5xl text-shark-950'>
           Portfolio
         </h2>
       </header>
-      <Project number='1' title='Serenity'/>
-      <Project direction='flex-row-reverse' number='2' title='Luminous'/>
-      <Project number='3' title='The Coffee Store'/>
+      <div className='md:grid md:grid-cols-2 mt-14'>
+        <Project number='1' title='Serenity' />
+        <Project number='2' title='Luminous' />
+        <Project number='3' title='The Coffee Store' />
+      </div>
     </section>
   );
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,10 @@
 import mockup from '../assets/macbook-mockup.avif';
 import Role from './Role';
 
-export default function Project({ direction, number, title }) {
+export default function Project({ number, title }) {
     return (
         <article>
-            <header className={`flex justify-between items-baseline px-5 py-2 ${direction}`}>
+            <header className={`flex flex-row-reverse justify-between items-baseline px-5 py-2`}>
                 <h3 className="text-4xl text-shark-950 font-playFair">{title}</h3>
                 <sm className='font-lora text-9xl'>{number}.</sm>
             </header>
